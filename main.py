d4afe37f52f406b9613d29f6cb798065ef59201c
ya-termenator <ya.lishinskiy2017@gmail.com>
2020-08-27 17:46:49 +0300

    squares for names were added + changed window size

--- main.py
+++ main.py
@@ -1,10 +1,11 @@
 import sys, pygame
 pygame.init()
 
-size = width, height = 800, 800
+size = width, height = 1020, 820
 white = 255, 255, 255
 black = 133, 133, 133
 red = 250, 0, 0
+brown = 153, 51, 51
 
 screen = pygame.display.set_mode(size)
 
@@ -23,6 +24,21 @@ for i in range(64):
         x = 0
         n = not n
 
+# рисуем название ячеек
+x, y = 0, 800
+for i in range(8):
+    pygame.draw.rect(screen, white, (x, y, 100, 20))
+    pygame.draw.rect(screen, brown, (x, y, 98, 20),4)
+    x += 100
+
+x, y = 800, 0
+for i in range(8):
+    pygame.draw.rect(screen, white, (x, y, 20, 100))
+    pygame.draw.rect(screen, brown, (x, y, 20, 98),4)
+    y += 100
+pygame.draw.rect(screen, brown, (798, 798, 24, 23))
+
+
 # загружаем картинки
 checker_white = pygame.image.load('images/white.png')
 checker_black = pygame.image.load('images/black.png')
@@ -30,6 +46,10 @@ checker_black = pygame.image.load('images/black.png')
 # уменьшаем их в размере
 checker_white = pygame.transform.scale(checker_white, (100, 100))
 checker_black = pygame.transform.scale(checker_black, (100, 100))
+
+
+class Checker:
+    pass
 
 
 
