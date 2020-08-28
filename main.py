2d59305cb2478d6b7c06b75ea05bf517a184f904
ya-termenator <ya.lishinskiy2017@gmail.com>
2020-08-28 13:24:38 +0300

    player's turns is one by one. We make our choosen checkers colored

--- main.py
+++ main.py
@@ -3,8 +3,9 @@ pygame.init()
 
 size = width, height = 1130, 830
 white = 255, 255, 255
-grey = 133, 133, 133
 black = 0, 0, 0
+grey = 133, 133, 133
+green = 50, 205, 50
 red = 250, 0, 0
 brown = 153, 51, 51
 
@@ -34,7 +35,7 @@ x, y = 0, 800
 for i in range(8):
     pygame.draw.rect(screen, white, (x, y, 100, 30))
     pygame.draw.rect(screen, brown, (x, y, 98, 30), 4)
-    t1 = f1.render(x_names[i], 0, (black))
+    t1 = f1.render(x_names[i], 0, black)
     screen.blit(t1, (x + 44, y))
     x += 100
 
@@ -42,12 +43,10 @@ x, y = 800, 0
 for i in range(8):
     pygame.draw.rect(screen, white, (x, y, 30, 100))
     pygame.draw.rect(screen, brown, (x, y, 30, 98), 4)
-    t1 = f1.render(str(i+1), 0, (black))
+    t1 = f1.render(str(i+1), 0, black)
     screen.blit(t1, (x+7, y+40))
     y += 100
 pygame.draw.rect(screen, brown, (798, 798, 34, 32))
-
-
 
 # загружаем картинки
 checker_white = pygame.image.load('images/white.png')
@@ -58,7 +57,7 @@ checker_white = pygame.transform.scale(checker_white, (100, 100))
 checker_black = pygame.transform.scale(checker_black, (100, 100))
 
 
-class Checker():
+class Checker:
     def __init__(self, name, checker_color, border, queen, x, y):
         self.name = name
         self.checker_color = checker_color
@@ -68,15 +67,13 @@ class Checker():
         self.y = y
 
     def checked(self):
-        pass
+        pygame.draw.rect(screen, green, (self.x, self.y, 99, 99), 2)
 
     def ris_checker(self):
         pass
 
     def ris_block(self):
         pass
-
-
 
 
 names1 = ['b1', 'd1', 'f1', 'h1',
@@ -89,9 +86,9 @@ names1 = ['b1', 'd1', 'f1', 'h1',
           'a8', 'c8', 'e8', 'g8']
 
 # checker_color
-# 0 - пустой
-# 1 - белый
-# 2 - чорный
+# 2 - пустой
+# 0 - белый
+# 1 - чорный
 # border - 1/0
 # queen - 1/0
 
@@ -108,9 +105,9 @@ for i in range(32):
         arr.append(Checker(names1[i], 1, 0, 0, x_coord, y_coord))
         screen.blit(checker_black, (x_coord, y_coord))
     elif i < 20:
-        arr.append(Checker(names1[i], 0, 0, 0, x_coord, y_coord))
-    else:
         arr.append(Checker(names1[i], 2, 0, 0, x_coord, y_coord))
+    else:
+        arr.append(Checker(names1[i], 0, 0, 0, x_coord, y_coord))
         screen.blit(checker_white, (x_coord, y_coord))
     x_coord += 200
     if x_coord > 700:
@@ -118,26 +115,31 @@ for i in range(32):
         x_coord = 0
 
 
-def find_exemplar(x, y):
+def find_exemplar(x, y, h):
     x, y = (x // 100) * 100, (y // 100) * 100
-    print(x, y)
     for i in arr:
-        if i.x == x and i.y == y:
+        if i.x == x and i.y == y and i.checker_color == h:
             return i
 
-
-
 # главный цикл
+# False  - ходит белый
+# True - ходит чорный
 
+
+hod = False
 while 1:
     for event in pygame.event.get():
         if event.type == pygame.QUIT: sys.exit()
 
         if pygame.mouse.get_pressed() == (1, 0, 0):
             x, y = pygame.mouse.get_pos()
-            checker = find_exemplar(x, y)
+            checker = find_exemplar(x, y, hod)
             try:
                 print(checker.name)
+                checker.checked()
             except AttributeError:
-                print(0)
+                del checker
+
+
+
         pygame.display.update()
